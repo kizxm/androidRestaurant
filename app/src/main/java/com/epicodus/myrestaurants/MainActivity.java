25e606b5da44d7bd4c20c77ee56721abf02aa801
Esti Shay <esti.r.shay@gmail.com>
2017-10-12 11:04:05 -0700

    extended on click listeners

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.locationEditText) EditText mLocationEditText;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
@@ -28,16 +28,17 @@ public class MainActivity extends AppCompatActivity {
         Typeface sugarFont = Typeface.createFromAsset(getAssets(), "fonts/sugarvinegar.ttf");
         mAppNameTextView.setTypeface(sugarFont);
 
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
             @Override
             public void onClick(View v) {
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
+                if (v == mFindRestaurantsButton) {
+                    String location = mLocationEditText.getText().toString();
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    intent.putExtra("location", location);
+                    startActivity(intent);
+                }
             }
-        });
-    }
-}
+        }
 
 
