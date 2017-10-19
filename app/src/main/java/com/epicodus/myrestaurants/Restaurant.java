9ceae98395d8f80e0eec065dabb8ccca768ba4af
Guest User <Guest@Epicodus-4B.local>
2017-10-19 15:19:04 -0700

    json exceptions

---
+++ app/src/main/java/com/epicodus/myrestaurants/Restaurant.java
@@ -0,0 +1,66 @@
+package com.epicodus.myrestaurants;
+
+import java.util.ArrayList;
+
+
+
+public class Restaurant {
+    private String name;
+    private String phone;
+    private String website;
+    private double rating;
+    private String imageUrl;
+    private ArrayList<String> address = new ArrayList<>();
+    private ArrayList<String> categories = new ArrayList<>();
+    private double latitude;
+    private double longitude;
+
+public Restaurant(String name, String phone, String website, double rating, String imageUrl, ArrayList<String> address,
+                  ArrayList<String> categories, double latitude, double longitude) {
+    this.name = name;
+    this.phone = phone;
+    this.website = website;
+    this.rating = rating;
+    this.imageUrl = imageUrl;
+    this.address = address;
+    this.categories = categories;
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public ArrayList<String> getAddress() {
+        return address;
+    }
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -11,10 +11,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private static final String TAG = RestaurantsActivity.class.getSimpleName();
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
     private String[] restaurants = new String[] {"Mi Mero Mole", "Deschutes Brewery",
@@ -32,10 +38,8 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
         ButterKnife.bind(this);
 
-        MyRestaurantsArrayAdapter adapter = new MyRestaurantsArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants, cuisines);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         mListView.setAdapter(adapter);
-
-//        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -48,5 +52,27 @@ public class RestaurantsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the restaurants near: " + location);
+        getRestaurants(location);
+    }
+
+    private void getRestaurants(String location) {
+        final YelpService yelpService = new YelpService();
+        yelpService.findRestaurants(location, new Callback() {
+
+        @Override
+            public void onFailure(Call call, IOException e) {
+            e.printStackTrace();
+        }
+
+        @Override
+            public void onResponse(Call call, Response response) throws IOException {
+            try {
+                String jsonData = response.body().string();
+                Log.v(TAG, jsonData);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        });
     }
 }
